7b5d88239c9f249472509349a3899174dbc2db58
s-edwards <edwards@cs.vt.edu>
2006-02-19 19:06:50 +0000

    Importing project structure.

---
+++ src/net/sf/webcat/admin/FooPage.java
@@ -0,0 +1,74 @@
+/*==========================================================================*\
+ |  $Id$
+ |*-------------------------------------------------------------------------*|
+ |  Copyright (C) 2006 Virginia Tech
+ |
+ |  This file is part of Web-CAT.
+ |
+ |  Web-CAT is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU General Public License as published by
+ |  the Free Software Foundation; either version 2 of the License, or
+ |  (at your option) any later version.
+ |
+ |  Web-CAT is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU General Public License
+ |  along with Web-CAT; if not, write to the Free Software
+ |  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ |
+ |  Project manager: Stephen Edwards <edwards@cs.vt.edu>
+ |  Virginia Tech CS Dept, 660 McBryde Hall (0106), Blacksburg, VA 24061 USA
+\*==========================================================================*/
+
+package net.sf.webcat.admin;
+
+import com.webobjects.appserver.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.foundation.*;
+
+import net.sf.webcat.core.*;
+
+import org.apache.log4j.Logger;
+
+// -------------------------------------------------------------------------
+/**
+ * A property listing page.
+ *
+ *  @author edwards
+ *  @version $Id$
+ */
+public class FooPage
+    extends WOComponent
+{
+    //~ Constructors ..........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Creates a new PropertyList object.
+     * 
+     * @param context The context to use
+     */
+    public FooPage( WOContext context )
+    {
+        super( context );
+    }
+
+
+    // ----------------------------------------------------------
+    /* (non-Javadoc)
+     * @see com.webobjects.appserver.WOComponent#appendToResponse(com.webobjects.appserver.WOResponse, com.webobjects.appserver.WOContext)
+     */
+    public void appendToResponse( WOResponse arg0, WOContext arg1 )
+    {
+        log.debug( "appendToResponse()" );
+        // TODO Auto-generated method stub
+        super.appendToResponse( arg0, arg1 );
+    }
+
+    //~ Instance/static variables .............................................
+    static Logger log = Logger.getLogger( PropertyListPage.class );
+}
